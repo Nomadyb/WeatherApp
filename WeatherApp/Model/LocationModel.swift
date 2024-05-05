9af4658d4a6b1945d6d7a87acd37c575014aa9aa
Nomadyb <Nomadaeyb@outlook.com>
2024-05-06 00:39:19 +0300

    feat: Add FavoriteLocationViewModel.swift and FavoriteLocationView to project

---
+++ WeatherApp/Model/LocationModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationModel.swift
+//  WeatherApp
+//
+//  Created by Ahmet Emin Yalçınkaya on 5.05.2024.
+//
+
+import Foundation
---
+++ WeatherApp/View/LottieView.swift
@@ -0,0 +1,18 @@
+//
+//  LottieView.swift
+//  WeatherApp
+//
+//  Created by Ahmet Emin Yalçınkaya on 5.05.2024.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LottieView()
+}
