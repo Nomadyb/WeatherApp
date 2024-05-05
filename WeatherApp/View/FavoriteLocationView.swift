c469da8f85580ceea18226a12544184ed6b2b783
Nomadyb <Nomadaeyb@outlook.com>
2024-05-05 17:16:10 +0300

    feat: Add FavoriteLocationViewModel.swift and FavoriteLocationView to project
    This commit adds the `FavoriteLocationViewModel.swift` file and the `FavoriteLocationView` struct to the WeatherApp project. The `FavoriteLocationViewModel.swift` file is responsible for handling the logic related to favorite locations in the app, while the `FavoriteLocationView` struct is responsible for displaying the user's favorite locations. These additions are part of the ongoing development to enhance the user experience and provide more functionality in the WeatherApp. Additionally, the commit includes updates to the `WeatherModel` and `WeatherServiceModel` to improve the performance of the app by implementing lazy loading for weather icon images and enhancing the security of the API key.

--- WeatherApp/View/FavoriteLocationView.swift
+++ WeatherApp/View/FavoriteLocationView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct FavoriteLocationView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@StateObject var viewModel: FavoriteLocationViewModel
+	
+	var body: some View {
+		VStack {
+			Text("Hava Durumu")
+				.font(.title)
+				.padding(.top)
+			
+			SearchBar() // Arama kutusu
+			
+			Divider()
+				.padding(.top)
+			
+			ScrollView {
+				ForEach(viewModel.locations, id: \.name) { location in
+					LocationCard(location: location)
+						.padding(.horizontal)
+						.padding(.bottom)
+				}
+			}
+		}
+	}
+}
+
+
+//arama yapısı
+struct SearchBar: View {
+	@State private var SearchText = ""
+	
+	var body: some View {
+		/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+	}
+}
+
+
+
+// bir location card eklemeliyim
+struct LocationCard: View {
+	var location: Location
+	
+	
+	var body: some View {
+		/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+	}
 }
 
 #Preview {
-    FavoriteLocationView()
+	FavoriteLocationView()
 }
