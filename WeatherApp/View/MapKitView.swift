014104ede41f26570530b050c4c2e938e33559c4
Nomadyb <Nomadaeyb@outlook.com>
2024-05-06 11:54:35 +0300

    chore: Remove unused wallpaper_unix imageset file and add MapKitView.swift file , SplashScreen

---
+++ WeatherApp/View/MapKitView.swift
@@ -0,0 +1,18 @@
+//
+//  MapKitView.swift
+//  WeatherApp
+//
+//  Created by Ahmet Emin Yalçınkaya on 6.05.2024.
+//
+
+import SwiftUI
+
+struct MapKitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapKitView()
+}
